2d620ba9a6f73831235339698216fdca438df10f
jujuejundao <xu325504@hotmail.com>
2020-08-04 15:02:56 +0800

    1

--- src/baseline/convTasnet/hlt.sh
+++ src/baseline/convTasnet/hlt.sh
@@ -1,28 +1,26 @@
 #!/bin/sh
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 #SBATCH --output=hlt.txt
 #SBATCH --nodelist=ttnusa3
 
-gpu_id=2,3
 
 log_name='Conv_'$(date '+%d-%m-%Y(%H:%M:%S)')
-# mkdir logs/$log_name
+mkdir logs/$log_name
 
-CUDA_VISIBLE_DEVICES="$gpu_id" \
 python -W ignore \
 -m torch.distributed.launch \
---nproc_per_node=2 \
+--nproc_per_node=1 \
 --master_port=5556 \
 main.py \
 \
---train_dir '/data_a8/zexu/workspace/speech_separation/data/tr' \
---valid_dir '/data_a8/zexu/workspace/speech_separation/data/cv' \
---test_dir '/data_a8/zexu/workspace/speech_separation/data/tt' \
+--train_dir '/data07/zexu/workspace/speech_separation/data/tr' \
+--valid_dir '/data07/zexu/workspace/speech_separation/data/cv' \
+--test_dir '/data07/zexu/workspace/speech_separation/data/tt' \
 \
 --log_name $log_name \
 \
-# --use_tensorboard 1 \
-# >logs/$log_name/console.txt 2>&1
+--use_tensorboard 1 \
+>logs/$log_name/console.txt 2>&1
 
 # --continue_from ${continue_from} \
 
--- src/baseline/dprnn/hlt.sh
+++ src/baseline/dprnn/hlt.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 #SBATCH --output=hlt.txt
 #SBATCH --nodelist=ttnusa3
 
-gpu_id=2,3
 
-log_name='Conv_'$(date '+%d-%m-%Y(%H:%M:%S)')
+log_name='Dprnn_'$(date '+%d-%m-%Y(%H:%M:%S)')
 # mkdir logs/$log_name
 
-CUDA_VISIBLE_DEVICES="$gpu_id" \
 python -W ignore \
 -m torch.distributed.launch \
---nproc_per_node=2 \
+--nproc_per_node=1 \
 --master_port=5556 \
 main.py \
 \
---train_dir '/data_a8/zexu/workspace/speech_separation/data/tr' \
---valid_dir '/data_a8/zexu/workspace/speech_separation/data/cv' \
---test_dir '/data_a8/zexu/workspace/speech_separation/data/tt' \
+--batch_size 2 \
+\
+--train_dir '/data07/zexu/workspace/speech_separation/data/tr' \
+--valid_dir '/data07/zexu/workspace/speech_separation/data/cv' \
+--test_dir '/data07/zexu/workspace/speech_separation/data/tt' \
 \
 --log_name $log_name \
 \
