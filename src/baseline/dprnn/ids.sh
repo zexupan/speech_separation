d73a385eb6bd189e290fbd01a57d06e3c74c8db5
jujuejundao <xu325504@hotmail.com>
2020-08-04 17:50:29 +0800

    1

--- src/baseline/dprnn/ids.sh
+++ src/baseline/dprnn/ids.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-gpu_id=14,15
+gpu_id=0,2
 
 continue_from=''
 
 if [ -z ${continue_from} ]; then
 	log_name='dprnn_'$(date '+%d-%m-%Y(%H:%M:%S)')
-	mkdir logs/$log_name
+	# mkdir logs/$log_name
 else
 	log_name=${continue_from}
 fi
@@ -14,14 +14,15 @@ fi
 CUDA_VISIBLE_DEVICES="$gpu_id" \
 python -W ignore \
 -m torch.distributed.launch \
---nproc_per_node=2 \
---master_port=7886 \
+--nproc_per_node=1 \
+--master_port=9986 \
 main.py \
---batch_size 4 \
+--batch_size 2 \
 \
+--opt-level O1 \
 --log_name $log_name \
 \
---use_tensorboard 1 \
->logs/$log_name/console1.txt 2>&1
+# --use_tensorboard 1 \
+# >logs/$log_name/console1.txt 2>&1
 
 # --continue_from ${continue_from} \
