cf32ed5727f4ea09b1357326e50eb2dc8c13b190
jujuejundao <xu325504@hotmail.com>
2020-10-08 15:34:19 +0800

    dptnet

---
+++ src/baseline/dptNet/hlt.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+#SBATCH --gres=gpu:1
+#SBATCH --output=hlt.txt
+#SBATCH --nodelist=ttnusa4
+
+continue_from=
+
+if [ -z ${continue_from} ]; then
+	log_name='Dpa_'$(date '+%d-%m-%Y(%H:%M:%S)')
+	mkdir logs/$log_name
+else
+	log_name=${continue_from}
+fi
+
+python -W ignore \
+-m torch.distributed.launch \
+--nproc_per_node=2 \
+--master_port=5466 \
+main.py \
+\
+--train_dir '/home/zexu/workspace/speech_separation/data/tr' \
+--valid_dir '/home/zexu/workspace/speech_separation/data/cv' \
+--test_dir '/home/zexu/workspace/speech_separation/data/tt' \
+\
+--batch_size 8 \
+\
+--L 20 \
+--K 100 \
+\
+--log_name $log_name \
+--use_tensorboard 1 \
+>logs/$log_name/console.txt 2>&1
+
+# --continue_from ${continue_from} \
+
+# --opt-level O0 \
+# --use_tensorboard 1 \
---
+++ src/baseline/dptNet/ids.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+gpu_id=0,2
+
+continue_from=''
+
+if [ -z ${continue_from} ]; then
+	log_name='dprnn_'$(date '+%d-%m-%Y(%H:%M:%S)')
+	# mkdir logs/$log_name
+else
+	log_name=${continue_from}
+fi
+
+CUDA_VISIBLE_DEVICES="$gpu_id" \
+python -W ignore \
+-m torch.distributed.launch \
+--nproc_per_node=2 \
+--master_port=1152 \
+main.py \
+--batch_size 2 \
+\
+--opt-level O1 \
+--log_name $log_name \
+\
+# --use_tensorboard 1 \
+# >logs/$log_name/console1.txt 2>&1
+
+# --continue_from ${continue_from} \
