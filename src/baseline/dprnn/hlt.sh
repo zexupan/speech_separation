3d433d72dde089139dd5545bde4e103cd54622c6
jujuejundao <xu325504@hotmail.com>
2020-10-10 16:51:31 +0800

    log

--- src/baseline/dprnn/hlt.sh
+++ src/baseline/dprnn/hlt.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=hlt.txt
 #SBATCH --nodelist=ttnusa4
 
-continue_from=
+continue_from='Dprnn_07-10-2020(00:35:11)'
 
 if [ -z ${continue_from} ]; then
 	log_name='Dprnn_'$(date '+%d-%m-%Y(%H:%M:%S)')
@@ -27,9 +27,10 @@ main.py \
 --L 20 \
 --K 100 \
 \
+--continue_from ${continue_from} \
 --log_name $log_name \
 --use_tensorboard 1 \
->logs/$log_name/console.txt 2>&1
+>logs/$log_name/console1.txt 2>&1
 
 # --continue_from ${continue_from} \
 
